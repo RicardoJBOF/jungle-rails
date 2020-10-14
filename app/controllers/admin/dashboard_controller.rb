012d3f23310b9c5915d562e2e0fdd15b2e77942f
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-10-14 16:37:31 -0400

    Bug: Missing Admin Security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with :name => ENV['secret_user'], :password => ENV['secret_password']
+
+  
   def show
   end
+
 end
