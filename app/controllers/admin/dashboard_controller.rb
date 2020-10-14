ad1e2938409850e7423650590c88c2e49da07ee7
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-10-14 17:39:37 -0400

    Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,8 +2,9 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with :name => ENV['secret_user'], :password => ENV['secret_password']
 
-  
   def show
+    @products = Product.all
+    @category = Category.all
   end
 
 end
