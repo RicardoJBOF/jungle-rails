734e220da61d47e66bb231d73130c0a353fc1dd1
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-10-20 22:03:26 +0000

    Feature Spec: Product Details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "Visitor product detail", type: :feature, js: true  do
+  
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -22,12 +22,18 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
+    first('.pull-right').click
+
+
     # DEBUG / VERIFY
     save_screenshot
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css('.main-img')
+
+    # DEBUG / VERIFY
+    save_screenshot
 
   end
 
-  
+
 end
